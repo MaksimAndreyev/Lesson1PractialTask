49e9fb8afa2b3a792d08ef057e2c1aeb2a986abd
maximandreev <maxands@yandex.ru>
2022-08-07 15:30:00 +0300

    Task 4 completed

---
+++ Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while (i <= N)
+{
+    if ((i+2) > N) Console.WriteLine(i);
+    else Console.Write(i + ", ");
+    i += 2;
+}
